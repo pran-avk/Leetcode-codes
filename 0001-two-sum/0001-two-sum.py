b107402577530ac04e624b15fdd43ebf6193736e
pran-avk <kp755505@gmail.com>
2026-08-12 10:22:35 +0530

    Time: 4 ms (48.14%), Space: 13.2 MB (53.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
         a={}
-        for i in range (len(nums)):
+        for i in range(len(nums)):
             b=target-nums[i]
             if b in a:
                 return [a[b],i]

bb9c078b734e0819a204db98463f8632df355a3f
pran-avk <kp755505@gmail.com>
2026-09-01 21:33:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.25%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -5,11 +5,11 @@ class Solution(object):
         tot=0
         if a==tot:
             return 0
-        else:
-            for i in range(1,len(nums)):
-                a-=nums[i]
-                tot+=nums[i-1]
-                if a==tot:
-                    return i
-            return -1
+        
+        for i in range(1,len(nums)):
+            a-=nums[i]
+            tot+=nums[i-1]
+            if a==tot:
+                return i
+        return -1
         
